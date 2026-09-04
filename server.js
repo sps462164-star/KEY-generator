cd8da15fea9e7e48a8697e204daafa8137b4e94b
sps462164-star <sps462164@gmail.com>
2026-09-05 02:04:25 +0530

    Change static file serving directory to root

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const path = require('path');
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
+
 
 // MongoDB Connection
 const MONGO_URI = process.env.MONGO_URI;
